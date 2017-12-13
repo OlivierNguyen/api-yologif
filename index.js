a2d5302a6c2b0e4d0f0d02f73ef1e9945ad38571
Olivier NGUYEN <olivier.nguyen@recast.ai>
2017-12-13 15:20:54 +0100

    remove some templates + add post route

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const app = express()
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  
+app.post('/', (req, res) => {
+  console.log(req.body)
+
+  res.send({
+    replies: [{
+      type: 'text',
+      content: 'Roger that',
+    }], 
+    conversation: {
+      memory: { key: 'value' }
+    }
+  })
+})
+  
+app.post('/errors', (req, res) => {
+  console.log(req.body) 
+  res.send() 
+}) 
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))  
