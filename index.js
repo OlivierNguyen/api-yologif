38d6e9999abc293a1d271789a543863e3e908065
Olivier NGUYEN <olivier.nguyen@recast.ai>
2017-12-13 16:06:44 +0100

    add bodyparser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const path = require("path");
 const axios = require("axios");
 const PORT = process.env.PORT || 5000;
