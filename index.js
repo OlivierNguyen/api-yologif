80193646fd5774735064e0fe06505ee4a6c33b75
Olivier NGUYEN <olivier.nguyen@recast.ai>
2017-12-13 15:54:09 +0100

    use API Giphy to get gif image

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const axios = require("axios");
+const PORT = process.env.PORT || 5000;
+const GIPHY_API_KEY = "LIfYQva4FvbRqRNpR7sI8wleRxv5azMn";
 
-const app = express()
+const app = express();
+
+const searchGiphy = search => {
+  return axios.get("http://api.giphy.com/v1/gifs/random", {
+    params: {
+      api_key: GIPHY_API_KEY,
+      tag: search
+    }
+  });
+};
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  
-app.post('/', (req, res) => {
-  console.log(req.body)
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"));
 
-  res.send({
-    replies: [{
-      type: 'text',
-      content: 'Roger that',
-    }], 
-    conversation: {
-      memory: { key: 'value' }
-    }
-  })
-})
-  
-app.post('/errors', (req, res) => {
-  console.log(req.body) 
-  res.send() 
-}) 
+app.post("/", (req, res) => {
+  console.log(req.body);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))  
+  searchGiphy("wtf").then(response => {
+    const data = response.data || {};
+
+    res.send({
+      replies: [
+        {
+          type: "picture",
+          content: data.data.image_url
+        }
+      ]
+    });
+  });
+});
+
+app.post("/errors", (req, res) => {
+  console.log(req.body);
+  res.send();
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
